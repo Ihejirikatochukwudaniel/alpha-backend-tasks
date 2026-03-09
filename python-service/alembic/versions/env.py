c89b876ebecf2060c34cf5d55b88c65afea14240
Tochukwu Ihejirika Daniel <129788810+Ihejirikatochukwudaniel@users.noreply.github.com>
2026-03-09 17:13:48 +0100

    Add files via upload

---
+++ python-service/alembic/versions/env.py
@@ -0,0 +1,49 @@
+import os
+import asyncio
+from logging.config import fileConfig
+
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import pool
+
+from alembic import context
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from app.models.briefing import Base
+
+config = context.config
+fileConfig(config.config_file_name)
+target_metadata = Base.metadata
+
+
+def run_migrations_offline():
+    url = os.environ["DATABASE_URL"]
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection):
+    context.configure(connection=connection, target_metadata=target_metadata)
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online():
+    url = os.environ["DATABASE_URL"]
+    connectable = create_async_engine(url, poolclass=pool.NullPool)
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+    await connectable.dispose()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    asyncio.run(run_migrations_online())
